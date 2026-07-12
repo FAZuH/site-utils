2b0f99a1cff2147986db7d547ea8fed261a021ba
FAZuH <mail@fazuh.com>
2026-07-12 15:39:36 +0700

    refactor(embed)!: Use string for HeadEmbed fields [pub]

--- src/embed.rs
+++ src/embed.rs
@@ -1,17 +1,12 @@
 use dioxus::prelude::*;
 
 #[component]
-pub fn HeadEmbed(
-    title: &'static str,
-    description: &'static str,
-    url: &'static str,
-    image: &'static str,
-) -> Element {
+pub fn HeadEmbed(title: String, description: String, url: String, image: String) -> Element {
     rsx! {
-        document::Meta { property: "og:title", content: title }
-        document::Meta { property: "og:description", content: description }
+        document::Meta { property: "og:title", content: title.clone() }
+        document::Meta { property: "og:description", content: description.clone() }
         document::Meta { property: "og:url", content: url }
-        document::Meta { property: "og:image", content: image }
+        document::Meta { property: "og:image", content: image.clone() }
         document::Meta { property: "og:type", content: "website" }
         document::Meta { name: "twitter:card", content: "summary_large_image" }
         document::Meta { name: "twitter:title", content: title }
